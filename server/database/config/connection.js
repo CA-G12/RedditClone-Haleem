93b555a02bbc2af255d361b4dcda5ad34e9b87a4
Abdelhaleem99 <haleemderawi@gmail.com>
2022-09-05 08:25:15 +0300

    built database relates #2 closes #2

---
+++ server/database/config/connection.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+require('env2')('.env');
+
+let DB_URL = '';
+
+if (process.env.NODE_ENV === 'test') {
+  DB_URL = process.env.DB_URL_TEST;
+} else if (process.env.NODE_ENV === 'dev') {
+  DB_URL = process.env.DB_URL;
+} else if (process.env.NODE_ENV === 'production') {
+  DB_URL = process.env.DATABASE_URL;
+} else throw new Error('NO DB_URL');
+
+module.exports = new Pool(options);
